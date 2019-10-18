89ac35e65ddd9150c87bc18d2674952eac68ce82
ehrrrr <elisaveta.s.hristova@gmail.com>
2019-10-18 14:11:43 +0300

    New client side organisation

---
+++ public/src/script.js
@@ -0,0 +1,12 @@
+// fetch('http://localhost:3000/word', {
+// 	method: 'POST',
+// 	headers: {
+// 		'Content-Type': 'application/json'
+// 	},
+// 	body: JSON.stringify({
+// 		word: 'apple'
+// 	})
+// })
+// 	.then((res) => res.json())
+// 	.then((data) => console.log(data))
+// 	.catch((err) => console.log('Error'));
