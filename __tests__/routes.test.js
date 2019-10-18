f8c40be0caaef5136957adee13f3c93912df2d59
ehrrrr <elisaveta.s.hristova@gmail.com>
2019-10-18 14:12:45 +0300

    Testing server

---
+++ __tests__/routes.test.js
@@ -0,0 +1,38 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('POST /word', function() {
+	it('responds with json', function(done) {
+		request(app)
+			.post('/word')
+			.send({ word: 'hello' })
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end(function(err, res) {
+				if (err) return done(err);
+				done();
+			});
+	});
+});
+
+describe('POST /sentence', function() {
+	it('responds with json', function(done) {
+		request(app)
+			.post('/sentence')
+			.send({ sentence: 'Hello world!' })
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end(function(err, res) {
+				if (err) return done(err);
+				done();
+			});
+	});
+});
+
+describe('GET /history', function() {
+	it('responds with json', function(done) {
+		request(app).get('/history').expect('Content-Type', /json/, done);
+	});
+});
