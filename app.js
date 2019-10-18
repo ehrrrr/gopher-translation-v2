89b109d76e92b2dfa255843aaf205082e7638bdf
ehrrrr <elisaveta.s.hristova@gmail.com>
2019-10-18 21:06:31 +0300

    Removing the unnecessary

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 const URL = process.env.ROOT_URL || 'http://localhost:3000/';
@@ -11,21 +10,12 @@ const sentenceRoutes = require('./routes/sentence');
 const historyRoutes = require('./routes/history');
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.set('view engine', 'ejs');
+app.use(express.json());
 
 app.use('/', indexRoutes);
 app.use('/word', wordRoutes);
 app.use('/sentence', sentenceRoutes);
 app.use('/history', historyRoutes);
-
-// app.use(function(req, res, next) {
-// 	res.header('Access-Control-Allow-Origin', URL + '/client'); // update to match the domain you will make the request from
-// 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-// 	next();
-// });
 
 app.listen(PORT, () => {
 	console.log(`Server has started! \nApp is running on port ${PORT}`);
